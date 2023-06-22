71831756a7a7593e3cbebbebd89b0b6e39c39839
Bharath <kumarbharath63@gmail.com>
2023-06-22 18:05:42 +0530

    Initial setup for learining go language

---
+++ main.go
@@ -0,0 +1,19 @@
+package main;
+import "fmt";
+var(
+ name = "Bharath kumar"
+ firstName = "Bharath"
+ lastName = "kumar"
+)
+ 
+
+func main(){
+	fmt.Println("hello")
+	fmt.Println(name)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	version string
+	fmt.Println(version)
+	
+}
