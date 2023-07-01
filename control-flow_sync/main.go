af03f4d5bd6be2097889da27863deabef632e495
Bharath <kumarbharath63@gmail.com>
2023-07-01 23:00:55 +0530

    comments and workflow

--- control-flow_sync/main.go
+++ control-flow_sync/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 type Server struct {
-	quitch chan struct{} // because struct{} is zero byte
+	quitch chan struct{} // because struct{} is zero bytec, bool is 1 or 2 bytes
 	msgch  chan string
 }
 
+// start a new instance of the server like a constructor gives an object
 func newServer() *Server {
 	return &Server{
 		quitch: make(chan struct{}),
@@ -30,14 +31,15 @@ func (s *Server) quit() error {
 }
 
 func (s *Server) loop() error {
-mainloop:
+mainloop: // if we break from the select the main for will run infinitely , so we mainloop(name anything) and break fully from the for and select
 	for {
 		select {
-		// why we don't assign the channel here for quitch is it is a struct just a signal
+		// why we don't assign the channel here for quitch is, it is a struct just a signal
 		case <-s.quitch:
 			fmt.Println("quitting server")
 			break mainloop
 		case msg := <-s.msgch:
+			fmt.Println("in msg processing")
 			s.handleMessage(msg)
 		}
 	}
@@ -53,12 +55,20 @@ func (s *Server) handleMessage(msg string) {
 }
 
 func main() {
+	/*
+		1. creates a new instances of a server
+		2.  starts the server
+		3. send msg hello world
+		4. delay server for 5 secs
+		5. quit the server
+	*/
 	serve := newServer()
 	go func() {
+		serve.sendMessage("hello world")
 		time.Sleep(time.Second * 5)
 		serve.quit()
 	}()
-
+	// starts sync and starts before the above go func as latter is non blocking async function
 	serve.start()
 
 }
